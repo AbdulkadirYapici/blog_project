d47a7fa536cae26149b214034ba52e5870ef0c24
kadir <kadiryapici60@gmail.com>
2019-11-05 10:30:47 +0300

    Sign in, Sign Up, Log Out Done

---
+++ src/Controller/Admin/RegistrationController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function register(AuthenticationUtils $authenticationUtils, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        //$error = $authenticationUtils->getLastAuthenticationError();
+        $error="";
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $error = $authenticationUtils->getLastAuthenticationError();
+
+            //var_dump($error); exit();
+            $newEmail= $form->get('email')->getData();
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $existEmail = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findByEmail($newEmail);
+
+            if($existEmail != NULL){
+                //throw new \Exception('Email already exists.');
+                $error = "Girilen email başka bir kullanıcı tarafından kullanılıyor." ;
+                return $this->render(
+                    'registration/register.html.twig',[
+
+                    'form' => $form->createView(),
+                    'error' => $error
+                ]);
+            }
+            else
+            {
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }
+
+            exit();
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render(
+            'registration/register.html.twig',[
+
+            'form' => $form->createView(),
+            'error' => $error
+        ]);
+    }
+}
